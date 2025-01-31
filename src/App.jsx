8452f252199c708f32a1add744092aae1757b347
Eliah <eliahmwelangi01@gmail.com>
2025-01-31 16:56:01 +0300

    useEffect and useState hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react"
+
 const Card = ({title}) => {
+  const [hasLiked, setHasLiked] = useState(false);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`${title} movie has been liked: ${hasLiked}`);
+  }, [hasLiked])
+
   return(
-    <div className="card">
-      <h2>Movie: {title}</h2>
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h2>Movie: {title} <br /> {count ? count : null}</h2>
+
+      <button onClick={() => setHasLiked((prevState) => !prevState)}>
+        { hasLiked ? 'Liked': 'Like' }
+      </button>
     </div>
   )
 }
