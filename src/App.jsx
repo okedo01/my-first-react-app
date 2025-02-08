0d7ec24ea901dca1f28578bf06eea6bfdc7dc1a1
Eliah <eliahmwelangi01@gmail.com>
2025-02-08 11:14:10 +0300

    Fetching Movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import Search from './components/search'
+import Search from './components/search';
+import MovieList from './components/MovieList';
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const API_OPTIONS = {
   method: 'GET',
-  header: {
-    accept: 'application/json',
+  headers: {
+    accept: "application/json",
     Authorization: `Bearer ${API_KEY}`
   }
 }
@@ -15,21 +16,29 @@ const API_OPTIONS = {
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState(null);
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const movieApp = () => {
+    setIsLoading(true);
+    setErrorMessage('');
+
     const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
     fetch(endpoint, API_OPTIONS)
       .then(res => {
-        if(!res.OK) {
+        if(!res.ok) {
           throw new Error("failed to load resources from the server");
         }
         return res.json()
       })
       .then(data => {
-        console.log(data);
+        setMovieList(data.results);
+        setIsLoading(false);
+        
       }).catch(err => {
         setErrorMessage(err.message);
+        setIsLoading(false);
       })
   }
 
@@ -43,14 +52,27 @@ function App() {
         <header>
           <img src="./hero.jpeg" alt="hero banner" />
           <h1>Find <span className='text-gradient'>Movies</span> you'll enjoy without the Hasstle</h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-        <h1>{searchTerm}</h1>
-        <div className='error'>{errorMessage}</div>
+        <section className="movie-list">
+          <h1>All Movies</h1>
+          
+          { isLoading ? (
+            <div className='text-white'>Loading...</div>
+          ) : errorMessage ? (
+            <div className='text-red-500'>{ errorMessage }</div>
+          ) : (
+            <ul>
+              { movieList.map((movie) => (
+                <p className='text-white'>{ movie.title }</p>
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
     </main>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,9 @@
+const MovieList = () => {
+    return (
+        <div className="text-white">
+            <p>Movies</p>
+        </div>
+    );
+}
+ 
+export default MovieList;
