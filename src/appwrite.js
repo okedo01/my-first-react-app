c961207c81de47739ce04b87233e0e286a169ef0
Eliah <eliahmwelangi01@gmail.com>
2025-02-10 11:44:29 +0300

    Appwrite getting started

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,7 +1,24 @@
+import { Client, Databases, Query } from "appwrite";
+
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
-export const updateSearchCount = () => {
-    console.log(PROJECT_ID, DATABASE_ID, COLLECTION_ID);
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(PROJECT_ID)
+
+const database = new Databases(client)
+
+export const updateSearchCount = async (searchTerm, movie) => {
+    //1. Use the appwrite SDK to check if the search term exists in the database
+try {
+    const result = await database.listDocuments(DATABASE_ID, PROJECT_ID, [
+        Query.equal('searchTerm', searchTerm),
+    ])
+} catch (error) {
+    console.log(error.message);
+}
+    //2. if it does, update the count
+    //3. if it doesn't, create a new document with the search term and count as 1 
 }
